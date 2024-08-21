52e9a5ae9bc28b24e1149cd65bd5ea525a125057
AboubacarSiriki <sidiksoum344@gmail.com>
2024-08-21 22:47:03 +0000

    Projet Spring

--- src/main/java/ci/digitalacademy/monetab/controller/UserControlleur.java
+++ src/main/java/ci/digitalacademy/monetab/controller/UserControlleur.java
@@ -1,5 +1,25 @@
 package ci.digitalacademy.monetab.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
+@RequestMapping("/Users")
 public class UserControlleur {
+
+    @GetMapping
+    public String showUserPage(){
+        return "/User/utilisateur";
+    }
+
+    @GetMapping("/addUser")
+    public String showAddUserPage(){
+        return "/User/form";
+    }
+
+    @GetMapping("/updateUser")
+    public String showUpdateUserPage(){
+        return "/User/form";
+    }
 }
